ba18ff666b9d1aa146f7647f7213e9ee8babee59
nick-roberson <nicholas.roberson.95@gmail.com>
2024-03-17 13:48:21 -0700

    add sample projects

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,19 +1,53 @@
 import React from 'react';
-import logo from './logo.svg';
 import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+import Link from '@mui/material/Link';
+
 import './App.css';
 
+// Define a project card class 
+class ProjectCard {
+  title: string;
+  description: string;
+  link: string;
+  constructor(title: string, description: string, link: string) {
+    this.title = title;
+    this.description = description;
+    this.link = link;
+  }
+}
+
 function App() {
+
+  // Create a list of projects
+  const projects: ProjectCard[] = [
+    new ProjectCard("Project 1", "Description 1", "Link 1"),
+    new ProjectCard("Project 2", "Description 2", "Link 2"),
+    new ProjectCard("Project 3", "Description 3", "Link 3"),
+  ];
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Learn more about my personal projects!
         </p>
-        <Button variant="contained" color="primary">
-          Click me!
-        </Button>
+
+        <div>
+          {projects.map((project) => {
+            return (
+              <div>
+                <h1>{project.title}</h1>
+                <p>{project.description}</p>
+                <Button variant="contained" href={project.link}>
+                  Learn More
+                </Button>
+              </div>
+            );
+          })}
+        </div>
       </header>
     </div>
   );
